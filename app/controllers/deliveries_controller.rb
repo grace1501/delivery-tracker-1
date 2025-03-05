4f5cca721ae999397d3af01a8bb60677550f99f2
Grace Do <61570746+grace1501@users.noreply.github.com>
2025-03-05 18:46:23 +0000

    changes by request. all codes worked

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -23,7 +23,7 @@ class DeliveriesController < ApplicationController
     the_delivery.description = params.fetch("query_description")
     the_delivery.details = params.fetch("query_details")
     the_delivery.supposed_to_arrive_on = params.fetch("query_supposed_to_arrive_on")
-    the_delivery.arrived = params.fetch("query_arrived", false)
+    the_delivery.arrived = false
 
     if the_delivery.valid?
       the_delivery.save
@@ -37,7 +37,7 @@ class DeliveriesController < ApplicationController
     the_id = params.fetch("path_id")
     the_delivery = Delivery.where({ :id => the_id }).at(0)
 
-    the_delivery.arrived = params.fetch("query_arrived")
+    the_delivery.arrived = true
 
     the_delivery.save
     redirect_to("/deliveries", { :notice => "Delivery updated successfully."} )
